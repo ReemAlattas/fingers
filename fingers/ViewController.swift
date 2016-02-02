bc17250dfca6287b02b4515424e884886fe1d85f
ReemAlattas <ralattas@live.com>
2016-02-02 14:25:40 -0500

    Fix layouts

--- fingers/ViewController.swift
+++ fingers/ViewController.swift
@@ -20,6 +20,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var guess: UITextField!
 
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func guessBtn(sender: AnyObject) {
+        var randomNumber = arc4random_uniform(6)
+        print(randomNumber)
+        var guessInt = Int(guess.text!)
+        print(guessInt)
+        if guessInt != nil {
+            
+            if Int(randomNumber) == guessInt {
+                result.text = "Correct!"
+            }
+            else {
+                result.text = "Nope! It was a \(randomNumber)"
+            }
+            
+        } else {
+            result.text = "Please enter a number 0-5"
+        }
+
+    }
 }
 
